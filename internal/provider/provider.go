138c0c5e9577b168a731b307c4d57f95ba5c825e
Sean Gillespie <sean.gillespie@temporal.io>
2023-08-09 16:32:30 -0700

    Connect to Temporal Cloud via API keys (#3)
    API keys are either specified in configuration or read from the TEMPORAL_CLOUD_API_KEY environment variable.

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,12 +2,14 @@ package provider
 
 import (
 	"context"
-	"net/http"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 // Ensure TerraformCloudProvider satisfies various provider interfaces.
@@ -23,6 +25,7 @@ type TerraformCloudProvider struct {
 
 // TerraformCloudProvider describes the provider data model.
 type TerraformCloudProviderModel struct {
+	APIKey types.String `tfsdk:"api_key"`
 }
 
 func (p *TerraformCloudProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
@@ -31,23 +34,43 @@ func (p *TerraformCloudProvider) Metadata(ctx context.Context, req provider.Meta
 }
 
 func (p *TerraformCloudProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
-	resp.Schema = schema.Schema{}
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"api_key": schema.StringAttribute{
+				Optional:  true,
+				Sensitive: true,
+			},
+		},
+	}
 }
 
 func (p *TerraformCloudProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
 	var data TerraformCloudProviderModel
-
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Configuration values are now available.
-	// if data.Endpoint.IsNull() { /* ... */ }
+	if data.APIKey.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("api_key"),
+			"Unknown Terraform Cloud API Key",
+			"The provider cannot create a Terraform Cloud API client as there is an unknown configuration value for the Temporal Cloud API Key."+
+				" Either apply the source of the value first, or statically set the API Key via environment variable or in configuration.")
+		return
+	}
 
-	// Example client configuration for data sources and resources
-	client := http.DefaultClient
+	apiKey := os.Getenv("TEMPORAL_CLOUD_API_KEY")
+	if !data.APIKey.IsNull() {
+		apiKey = data.APIKey.ValueString()
+	}
+
+	client, err := NewClient(apiKey)
+	if err != nil {
+		resp.Diagnostics.AddError("Failed to connect to Temporal Cloud API", err.Error())
+		return
+	}
+
 	resp.DataSourceData = client
 	resp.ResourceData = client
 }
