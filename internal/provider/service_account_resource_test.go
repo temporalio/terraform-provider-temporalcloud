5fc0a2dcea06357cd5f27d7ba389109fd54e7a28
Kshitij Wadhwa <kwadhwa@openai.com>
2024-09-24 18:10:34 -0700

    add service account resource

---
+++ internal/provider/service_account_resource_test.go
@@ -0,0 +1,163 @@
+package provider
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"testing"
+	"text/template"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+
+	cloudservicev1 "go.temporal.io/api/cloud/cloudservice/v1"
+)
+
+func createRandomName() string {
+	return fmt.Sprintf("%s-terraformprovider-name", randomString())
+}
+
+func TestAccBasicServiceAccount(t *testing.T) {
+	name := createRandomName()
+	config := func(name string, role string) string {
+		return fmt.Sprintf(`
+provider "temporalcloud" {
+
+}
+
+resource "temporalcloud_service_account" "terraform" {
+  name = "%s"
+  account_access = "%s"
+}`, name, role)
+	}
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: config(name, "Read"),
+			},
+			{
+				Config: config(name, "Developer"),
+			},
+			{
+				Config: config(name, "Admin"),
+			},
+		},
+	})
+}
+
+func TestAccBasicServiceAccountWithNamespaceAccesses(t *testing.T) {
+	type configArgs struct {
+		Name          string
+		NamespaceName string
+		AccountPerm   string
+		NamespacePerm string
+	}
+
+	name := createRandomName()
+
+	tmpl := template.Must(template.New("config").Parse(`
+provider "temporalcloud" {
+
+}
+
+resource "temporalcloud_namespace" "test" {
+  name               = "{{ .NamespaceName }}"
+  regions            = ["aws-us-east-1"]
+  accepted_client_ca = base64encode(<<PEM
+-----BEGIN CERTIFICATE-----
+MIIBxjCCAU2gAwIBAgIRAlyZ5KUmunPLeFAupDwGL8AwCgYIKoZIzj0EAwMwEjEQ
+MA4GA1UEChMHdGVzdGluZzAeFw0yNDA4MTMyMzQ2NThaFw0yNTA4MTMyMzQ3NTha
+MBIxEDAOBgNVBAoTB3Rlc3RpbmcwdjAQBgcqhkjOPQIBBgUrgQQAIgNiAARG+EuL
+uKRsNWs7Rbz6ciaJQB7QINTRLmTgGGE8H/wAs+KjvctjPdDdqFPZrxShRY3PUdk2
+pgQKRugMTe3N52pxBx4Iablz8felfdv4kyLQbdsJzY9XmCYX3D68/9Hxsl2jZzBl
+MA4GA1UdDwEB/wQEAwIBhjAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBSYC5/u
+K78bK1M8Fv1M6ELMjF2ZMDAjBgNVHREEHDAaghhjbGllbnQucm9vdC50ZXN0aW5n
+LjBycDUwCgYIKoZIzj0EAwMDZwAwZAIwSycjxxmYTgV5eSJbaGMINr5LQgyKQUHQ
+ryBKSGLKASa/e2ntyhsqRhj77gJ8DmkZAjAIlpDacF+Sq1kpZ5tMV7ZLElcujzj4
+US8pEmNuIiCguEGwi+pb5CWfabETEHApxmo=
+-----END CERTIFICATE-----
+PEM
+  )
+
+  retention_days = 7
+}
+
+resource "temporalcloud_service_account" "terraform" {
+	  name = "{{ .Name }}"
+  account_access = "{{ .AccountPerm }}"
+  namespace_accesses = [
+    {
+      namespace_id = temporalcloud_namespace.test.id
+      permission = "{{ .NamespacePerm }}"
+    }
+  ]
+  
+  depends_on = [temporalcloud_namespace.test]
+}`))
+
+	config := func(args configArgs) string {
+		var buf bytes.Buffer
+		writer := bufio.NewWriter(&buf)
+		if err := tmpl.Execute(writer, args); err != nil {
+			t.Errorf("failed to execute template:  %v", err)
+			t.FailNow()
+		}
+
+		writer.Flush()
+		return buf.String()
+	}
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: config(configArgs{
+					Name:          name,
+					NamespaceName: randomString(),
+					NamespacePerm: "Write",
+					AccountPerm:   "Read",
+				}),
+				Check: func(state *terraform.State) error {
+					id := state.RootModule().Resources["temporalcloud_service_account.terraform"].Primary.Attributes["id"]
+					conn := newConnection(t)
+					serviceAccount, err := conn.GetServiceAccount(context.Background(), &cloudservicev1.GetServiceAccountRequest{
+						ServiceAccountId: id,
+					})
+					if err != nil {
+						return fmt.Errorf("failed to get Service Account: %v", err)
+					}
+					nsID := state.RootModule().Resources["temporalcloud_namespace.test"].Primary.Attributes["id"]
+					ns, err := conn.GetNamespace(context.Background(), &cloudservicev1.GetNamespaceRequest{
+						Namespace: nsID,
+					})
+					if err != nil {
+						return fmt.Errorf("failed to get namespace: %v", err)
+					}
+					spec := serviceAccount.ServiceAccount.GetSpec()
+					if strings.ToLower(spec.GetAccess().GetAccountAccess().GetRole()) != "read" {
+						return errors.New("expected account role to be read")
+					}
+					nsPerm, ok := spec.GetAccess().GetNamespaceAccesses()[ns.Namespace.GetNamespace()]
+					if !ok {
+						return fmt.Errorf("expected entry in NamespaceAccesses for namespace %s", ns.Namespace.GetNamespace())
+					}
+					if strings.ToLower(nsPerm.GetPermission()) != "write" {
+						return errors.New("expected namespace access permission to be write")
+					}
+					return nil
+				},
+			},
+		},
+	})
+}
