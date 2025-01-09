87b979326d763fe8e2f024ac609437b547515022
Brian Kassouf <briankassouf@users.noreply.github.com>
2025-01-09 12:40:10 -0800

    Create test cert for test

---
+++ internal/provider/testing.go
@@ -0,0 +1,55 @@
+package provider
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"time"
+)
+
+func generateTestCACertificate(org string) (caPEM []byte, err error) {
+	limit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, limit)
+	if err != nil {
+		return nil, fmt.Errorf("error generating serial number: %w", err)
+	}
+
+	now := time.Now().UTC()
+	conf := &x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{org},
+		},
+		NotBefore:             now.Add(-time.Minute), // grace of 1 min
+		NotAfter:              now.Add(time.Hour),
+		IsCA:                  true,
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCRLSign | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	key, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	if err != nil {
+		return nil, fmt.Errorf("unable to generate key: %w", err)
+	}
+
+	cert, err := x509.CreateCertificate(rand.Reader, conf, conf, key.PublicKey, key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate certificate: %w", err)
+	}
+	caPEMBuffer := new(bytes.Buffer)
+	err = pem.Encode(caPEMBuffer, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: cert,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return caPEMBuffer.Bytes(), nil
+}
