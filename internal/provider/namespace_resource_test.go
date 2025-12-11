c4f827bad3ef57dcd861ae23724ee35b248d69d4
Abhinav Nekkanti <10552725+anekkanti@users.noreply.github.com>
2025-12-11 15:33:22 -0800

    Fix test assertion

--- internal/provider/namespace_resource_test.go
+++ internal/provider/namespace_resource_test.go
@@ -879,8 +879,8 @@ PEM
 								return fmt.Errorf("expected provisioned capacity, got nil")
 							} else {
 								value := ns.GetNamespace().GetCapacity().GetProvisioned().GetCurrentValue()
-								if value != 2.0 {
-									return fmt.Errorf("expected provisioned capacity of 2, got %f", value)
+								if value != 1.0 {
+									return fmt.Errorf("expected provisioned capacity of 1, got %f", value)
 								}
 								// success
 								return nil
