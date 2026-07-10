b868387af0d4e51db6759a2cb0006af6d56421a6
Jeri Lane <jeri.lane@temporal.io>
2026-07-10 10:34:39 -0700

    Unify Access attributes between User and GroupAccess resources (#423)
    * Unify Access attributes between User and GroupAccess resources
    
    * Clarify account_access documentation

--- internal/provider/group_access_resource.go
+++ internal/provider/group_access_resource.go
@@ -106,7 +106,7 @@ func (r *groupAccessResource) Schema(_ context.Context, _ resource.SchemaRequest
 		},
 	}
 
-	addAccessSchemaAttrs(s)
+	addAccessSchemaAttrs(&s, "")
 	resp.Schema = s
 }
 
@@ -179,7 +179,6 @@ func (r *groupAccessResource) Create(ctx context.Context, req resource.CreateReq
 	}
 
 	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
-
 }
 
 func (r *groupAccessResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
@@ -322,7 +321,6 @@ func (r *groupAccessResource) Delete(ctx context.Context, req resource.DeleteReq
 		ResourceVersion:  currentGroup.GetGroup().GetResourceVersion(),
 		AsyncOperationId: uuid.New().String(),
 	})
-
 	if err != nil {
 		switch status.Code(err) {
 		case codes.NotFound:
