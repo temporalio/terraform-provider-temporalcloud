b6d3ad336442306f8230f36acddb105331d419b7
Shakeel Rao <shakeelrao79@gmail.com>
2026-08-21 15:41:32 -0400

    fix

--- examples/resources/temporalcloud_project/resource.tf
+++ examples/resources/temporalcloud_project/resource.tf
@@ -14,7 +14,6 @@ resource "temporalcloud_project" "payments" {
   display_name = "payments"
   description  = "Namespaces and resources owned by the Payments team."
 
-  // Set enable_delete_protection = true to block deletion. Destroying such a
+  // Add enable_delete_protection = true to block deletion. Destroying a protected
   // Project then takes two steps: set it back to false, apply, then destroy.
-  // enable_delete_protection = false
 }
